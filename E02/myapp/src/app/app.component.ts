3c7ac3e2a0089f42e8afa4e6feaa129e59545b53
Suyog <109020086+suyogchougale3096@users.noreply.github.com>
2024-05-02 10:23:45 +0530

    First Commit

---
+++ E02/myapp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'myapp';
+  imageUrl =
+    'https://imgd.aeplcdn.com/1280x720/n/cw/ec/53859/urus-performante-exterior-right-front-three-quarter-4.jpeg';
+
+  displayUrl(): string {
+    return this.imageUrl;
+  }
+
+  text: string = '';
+
+  display(): void {
+    console.log('Hello World!');
+  }
+}
